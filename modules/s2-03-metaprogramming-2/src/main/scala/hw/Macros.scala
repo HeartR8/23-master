b07111721db6bfc8f10b793a967080e8571ccd05
d.osokin <d.osokin@tinkoff.ru>
2024-03-17 21:21:25 +0300

    task done

--- modules/s2-03-metaprogramming-2/src/main/scala/hw/Macros.scala
+++ modules/s2-03-metaprogramming-2/src/main/scala/hw/Macros.scala
@@ -1,7 +1,8 @@
 // Файл для реализации макросов использующихся в Task.scala
 package hw
 
-import scala.quoted.{Quotes, Expr, quotes, ToExpr}
+import scala.quoted.{Expr, Quotes, ToExpr, quotes}
+import scala.util.Try
 import scala.util.matching.Regex
 
 // Макрос для задания II.1
@@ -10,7 +11,22 @@ object SafeRegex:
     ${ SafeRegexMacro.parse('str) }
 
 object SafeRegexMacro:
-  def parse(str: Expr[String])(using Quotes): Expr[Regex] = ???
+  def parse(str: Expr[String])(using Quotes): Expr[Regex] =
+    import quotes.reflect.report
+    str.value match {
+      case Some(value) =>
+        Try {
+          new scala.util.matching.Regex(value)
+        }.fold(
+          _ => report.errorAndAbort(s"Wrong regex: $value", str),
+          x => Expr(x)
+        )
+      case None => '{ new scala.util.matching.Regex($str) }
+    }
+
+  given ToExpr[Regex] with
+    def apply(x: Regex)(using Quotes): Expr[Regex] =
+      '{ new scala.util.matching.Regex(${ Expr(x.pattern.pattern()) }) }
 
 //-----------------------------------------------------------------------------
 // Макрос для задания II.2
@@ -21,4 +37,7 @@ object SourceFilePosition:
   inline def get: SourceFilePosition = ${ SourceFileMacro.getSourceFilePosition }
 
 object SourceFileMacro:
-  def getSourceFilePosition(using qctx: Quotes): Expr[SourceFilePosition] = ???
+  def getSourceFilePosition(using qctx: Quotes): Expr[SourceFilePosition] = {
+    import quotes.reflect._
+    '{ SourceFilePosition(${ Expr(Position.ofMacroExpansion.sourceFile.name) }, ${ Expr(Position.ofMacroExpansion.endLine) }) }
+  }
