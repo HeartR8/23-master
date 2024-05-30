a1fda6d0874af79e800570913fb756cd229135e5
d.osokin <d.osokin@tinkoff.ru>
2024-05-30 15:33:03 +0300

    hw done

--- modules/s2-05-applicatives-traverse/src/main/scala/hw/traverse/FormLocalization.scala
+++ modules/s2-05-applicatives-traverse/src/main/scala/hw/traverse/FormLocalization.scala
@@ -1,6 +1,8 @@
 package hw.traverse
 
-import cats.Applicative
+import cats.{Applicative, Eval, Traverse}
+import cats.syntax.traverse._
+import cats.syntax.apply._
 
 import java.time.ZonedDateTime
 
@@ -22,12 +24,31 @@ trait Localization[F[_]]:
   def localize: Template => F[Rendered]
 
 case class Form[A](
-    title: A,
-    tags: List[A],
-    subForms: List[Form[A]],
-    date: ZonedDateTime
-)
+                    title: A,
+                    tags: List[A],
+                    subForms: List[Form[A]],
+                    date: ZonedDateTime
+                  )
+
+object FormTraverse:
+  given Traverse[Form] with
+    override def traverse[G[_]: Applicative, A, B](fa: Form[A])(f: A => G[B]): G[Form[B]] =
+      val titleF = f(fa.title)
+      val tagsF = fa.tags.traverse(f)
+      val subFormsF = fa.subForms.traverse(traverse(_)(f))
+      (titleF, tagsF, subFormsF).mapN((title, tags, subForms) => Form(title, tags, subForms, fa.date))
+
+    override def foldLeft[A, B](fa: Form[A], b: B)(f: (B, A) => B): B =
+      val b1 = f(b, fa.title)
+      val b2 = fa.tags.foldLeft(b1)(f)
+      fa.subForms.foldLeft(b2)((acc, form) => foldLeft(form, acc)(f))
+
+    override def foldRight[A, B](fa: Form[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+      val lb1 = f(fa.title, lb)
+      val lb2 = fa.tags.foldRight(lb1)((tag, acc) => f(tag, acc))
+      fa.subForms.foldRight(lb2)((form, acc) => foldRight(form, acc)(f))
 
 object FormLocalization:
-
-  def localize[F[_]: Applicative: Localization](f: Form[Template]): F[Form[Rendered]] = ???
+  import FormTraverse.given
+  def localize[F[_]: Applicative](form: Form[Template])(using loc: Localization[F]): F[Form[Rendered]] =
+    form.traverse(template => loc.localize(template))
